ac584834bc0568800f6a9abe8d627c8040699af9
Rainmaker <k45h66m90>
2018-01-16 14:16:55 +0100

    mal sehen ob das funktioniert v1

---
+++ angulartest/src/app/kompeins/kompeins.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kompeins',
+  templateUrl: './kompeins.component.html',
+  styleUrls: ['./kompeins.component.css']
+})
+export class KompeinsComponent{
+
+	title = 'dies ist ein erster test';
+
+}
